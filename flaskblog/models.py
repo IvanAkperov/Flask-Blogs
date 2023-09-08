28d5a0f8690dcd6047266eac63bd293d31e60188
Ivan_The_Creator <akperovivan@yandex.ru>
2023-09-08 12:54:07 +0300

    models for users, posts and comments

---
+++ flaskblog/models.py
@@ -0,0 +1,56 @@
+from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
+from flaskblog import db, login_manager
+from datetime import datetime
+from flask_login import UserMixin
+from flask import current_app
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(24), unique=True, nullable=False)
+    email_field = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password_field = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+    def get_reset_token(self):
+        s = Serializer(current_app.config["SECRET_KEY"])
+        return s.dumps({"user_id": self.id})
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(current_app.config["SECRET_KEY"])
+        try:
+            user_id = s.loads(token, max_age=1800)["user_id"]
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"User: {self.user}, {self.email_field}, {self.image_file}"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    comments = db.relationship("Comment", backref="post")
+
+    def __repr__(self):
+        return f"Post: {self.title}, {self.date_posted}"
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    author = db.relationship('User', backref=db.backref('comments', lazy=True))
